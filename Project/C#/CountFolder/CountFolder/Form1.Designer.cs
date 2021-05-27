2da758752bafafe6cd6e23ee8ec63d8229e1f348
namtao <vanam.pxhn@gmail.com>
2021-05-27 14:35:51 +0700

    Library

--- Project/C#/CountFolder/CountFolder/Form1.Designer.cs
+++ Project/C#/CountFolder/CountFolder/Form1.Designer.cs
@@ -29,49 +29,35 @@ namespace CountFolder
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtPath = new System.Windows.Forms.TextBox();
-            this.btnExe = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
-            // txtPath
+            // dateTimePicker1
             // 
-            this.txtPath.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPath.Location = new System.Drawing.Point(12, 12);
-            this.txtPath.Multiline = true;
-            this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(443, 52);
-            this.txtPath.TabIndex = 0;
-            // 
-            // btnExe
-            // 
-            this.btnExe.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExe.Location = new System.Drawing.Point(485, 12);
-            this.btnExe.Name = "btnExe";
-            this.btnExe.Size = new System.Drawing.Size(92, 52);
-            this.btnExe.TabIndex = 1;
-            this.btnExe.Text = "Thực hiện";
-            this.btnExe.UseVisualStyleBackColor = true;
-            this.btnExe.Click += new System.EventHandler(this.btnExe_Click);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(161, 30);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 81);
-            this.Controls.Add(this.btnExe);
-            this.Controls.Add(this.txtPath);
+            this.ClientSize = new System.Drawing.Size(186, 56);
+            this.Controls.Add(this.dateTimePicker1);
             this.Name = "Form1";
-            this.Text = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtPath;
-        private System.Windows.Forms.Button btnExe;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
